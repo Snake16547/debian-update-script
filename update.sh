6f6144b2df05381b2fec4a13087450c3b5df7bc8
Snake16547 <32037245+Snake16547@users.noreply.github.com>
2023-02-16 20:28:32 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,36 +4,50 @@
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
+BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-# Clear the screen
+# Clear screen
 clear
 
-# ASCII art
-cat << "EOF"
-        ___     __________  _______   ________  _________   
-       /   \   /  _____/  \/  /    |  \_____  \ \_   ___ \  
-      /  ^  \ /   \  ____\  /|   |  |   /   |   \/    \  \/  
-     /  /_\  \\    \_\  \   \|   |  |  /    |    \     \____ 
-    /  _____  \\______  /___\ \___|  |__\_______  /\______  /
-    \/     \/        \/     \/                 \/        \/ 
+# Confirmation prompt
+read -p "This script will update your Debian system. Do you want to continue? (y/n): " choice
 
-EOF
+if [[ $choice =~ ^[yY](es)?$ ]]; then
+    # Update the package list and redirect output to null
+    echo -e "${GREEN}Updating package list...${NC}"
+    sudo apt update > /dev/null 2>&1
 
-# Update the package list and redirect output to null
-echo -e "${GREEN}Updating package list...${NC}"
-sudo apt update > /dev/null 2>&1
+    # Get the list of packages to be upgraded
+    echo -e "${GREEN}Checking for available upgrades...${NC}"
+    upgrade_list=$(apt list --upgradable 2>/dev/null | grep -v -e 'Listing...' -e 'packages are already' | cut -d '/' -f 1)
+    if [ -z "$upgrade_list" ]; then
+        echo -e "${YELLOW}No upgrades available.${NC}"
+    else
+        # Print the list of packages to be upgraded
+        echo -e "${GREEN}The following packages will be upgraded:${NC}"
+        echo -e "${BLUE}$upgrade_list${NC}"
+    fi
 
-# Upgrade the installed packages with detailed and animated output
-echo -e "${GREEN}Upgrading installed packages...${NC}"
-echo -e "${BLUE}[${NC}                    ${BLUE}]${NC} 0% completed"
-sudo apt upgrade -y 2>&1 | while read -r line; do
-    echo -e "${BLUE}[${NC}${line:0:20}${BLUE}]${NC} ${line:20}"
-done | whiptail --title "Updating Packages" --gauge "Please wait..." 8 50 0
+    # Upgrade the installed packages with detailed and animated output
+    echo -e "${GREEN}Upgrading installed packages...${NC}"
+    last_line=""
+    while true; do
+        new_line=$(sudo apt upgrade -y --quiet --assume-yes --fix-missing 2>&1 | tail -n 1)
+        if [ "$new_line" == "$last_line" ]; then
+            break
+        fi
+        last_line="$new_line"
+        echo -ne "${BLUE}$last_line\r${NC}"
+        sleep 0.1
+    done
 
-# Remove unnecessary packages and clean up and redirect output to null
-echo -e "${GREEN}Removing unnecessary packages and cleaning up...${NC}"
-sudo apt autoremove -y > /dev/null 2>&1
-sudo apt autoclean > /dev/null 2>&1
+    # Remove unnecessary packages and clean up and redirect output to null
+    echo -e "${GREEN}Removing unnecessary packages and cleaning up...${NC}"
+    sudo apt autoremove -y > /dev/null 2>&1
+    sudo apt autoclean > /dev/null 2>&1
 
-echo -e "${YELLOW}Done!${NC}"
+    echo -e "${GREEN}The update process is complete.${NC}"
+else
+    echo -e "${YELLOW}The update process has been canceled.${NC}"
+fi
